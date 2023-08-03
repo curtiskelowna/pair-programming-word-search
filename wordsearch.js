eb8777fd883fc13bc58bac5d4072b57f3989ebc1
Curtis Platt <134568869+curtiskelowna@users.noreply.github.com>
2023-08-03 15:49:22 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+const transpose = function(letters) {
+  let newArray = [];
+  for (let i = 0; i < letters.length; i++) {
+      let newRow = [];
+      for (let j = 0; j < letters.length; j++) {
+          newRow.push(letters[j][i]);
+      }
+      newArray.push(newRow);
+  }
+  return newArray;
 }
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (wordSearch === []) {
+      return "please add an array"
+  };
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+      if (l.includes(word)) 
+      return true;
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (l of verticalJoin) {
+      if (l.includes(word)) 
+      return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
